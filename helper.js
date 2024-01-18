99ecbcc524b7bc91d6dece4898a25b2837aacbb1
unknown <basil.diouf@gmail.com>
2024-01-18 11:42:13 +0100

    add sequeqlize and first model creation

--- helper.js
+++ helper.js
@@ -3,7 +3,7 @@ exports.success = (message, data) => {
 };
 
 exports.getUniqueId = (pokemons) => {
-  const pokemonsId = pokemons.map((pokemon) => pokemon.id);
+  const pokemonsIds = pokemons.map((pokemon) => pokemon.id);
   const maxId = pokemonsIds.reduce((a, b) => Math.max(a, b));
   const uniqueId = maxId + 1;
   return uniqueId;
