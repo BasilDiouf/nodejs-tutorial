4724163d8f9bd2b66b7e79639976666307b93270
unknown <basil.diouf@gmail.com>
2024-01-29 23:39:30 +0100

    change PORT and Host

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -6,7 +6,7 @@ const UserModel = require("../models/user");
 const bcrypt = require("bcrypt");
 
 const sequelize = new Sequelize("pokedex", "root", "", {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "mariadb",
   dialectOptions: {
     timezone: "Etc/GMT-2",
